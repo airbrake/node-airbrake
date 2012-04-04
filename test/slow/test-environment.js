f6bf4058cf893dfe4894f970213c8dbef008f6e4
Rob Scott <rob.scott87@gmail.com>
2012-04-04 14:00:01 -0400

    better solution that allows customizing dev environments

--- test/slow/test-environment.js
+++ test/slow/test-environment.js
@@ -1,48 +1,39 @@
 var common = require('../common');
-var airbrake = require(common.dir.root).createClient(common.key);
-var sinon = require('sinon');
 var assert = require('assert');
 
-// store original environment for later
-var origEnv = process.env.NODE_ENV;
-
-// throw an error in development
-process.env.NODE_ENV = 'development';
-var devErr = new Error('this error was thrown in development');
-var devSpy = sinon.spy();
-airbrake.notify(devErr, devSpy);
-
-// throw an error in production
-process.env.NODE_ENV = 'production';
-var proErr = new Error('this error was thrown in production');
-var proSpy = sinon.spy();
-airbrake.notify(proErr, proSpy);
-
-// restore original environment
-process.env.NODE_ENV = origEnv;
+(function testAddingKeyToDevelopmentEnvironments() {
+  var airbrake = require(common.dir.root).createClient(common.key, 'dev');
+  airbrake.developmentEnvironments.push('dev');
+  airbrake.notify(new Error('this should not be posted to airbrake'), function(err, url) {
+    assert.equal(err, undefined);
+    assert.equal(url, undefined);
+  });
+})();
 
 
-process.on('exit', function() {
-  // ensure that both callbacks wer called
-  assert.ok(devSpy.called);
-  assert.ok(proSpy.called);
+(function testResettingDevelopmentEnvironments() {
+  var airbrake = require(common.dir.root).createClient(common.key, 'development');
+  airbrake.developmentEnvironments = [];
+  airbrake.notify(new Error('this should be posted to airbrake'), function(err, url) {
+    assert.equal(err, undefined);
+    assert.ok(/^http:\/\//.test(url));
+  });
+})();
 
-  // check for errors
-  var devErr = devSpy.args[0][0];
-  if (devErr) {
-    throw devErr;
-  }
 
-  var proErr = proSpy.args[0][0];
-  if (proErr) {
-    throw proErr;
-  }
+(function testDevelopmentEnviroment() {
+  var airbrake = require(common.dir.root).createClient(common.key, 'development');
+  airbrake.notify(new Error('this should not be posted to airbrake'), function(err, url) {
+    assert.equal(err, undefined);
+    assert.equal(url, undefined);
+  });
+})();
 
-  // ensure that development returns callback without url
-  var devUrl = devSpy.args[0][1];
-  assert.equal(devUrl, undefined);
 
-  // ensure that production returns callback with url
-  var proUrl = proSpy.args[0][1];
-  assert.ok(/^http:\/\//.test(proUrl));
-});
+(function testProductionEnviroment() {
+  var airbrake = require(common.dir.root).createClient(common.key, 'production');
+  airbrake.notify(new Error('this should be posted to airbrake'), function(err, url) {
+    assert.equal(err, undefined);
+    assert.ok(/^http:\/\//.test(url));
+  });
+})();
