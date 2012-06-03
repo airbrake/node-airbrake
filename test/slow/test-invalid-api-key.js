6c5985d0d9f56df6ce41756ce9d55cdd9c909d04
Felix Geisendörfer <felix@debuggable.com>
2012-06-03 18:25:06 +0200

    Re-enable and fix invalid-api-key test
    See 5939fea31b7809ca32ab4f28d35ff17ef619b486

--- test/slow/test-invalid-api-key.js
+++ test/slow/test-invalid-api-key.js
@@ -1,10 +1,8 @@
-return console.log('disabled - airbrake returns success codes for this now?');
-
 var common = require('../common');
 var airbrake = require(common.dir.root).createClient();
 var assert = require('assert');
 
 var myErr = new Error('test-notify');
 airbrake.notify(myErr, function(err) {
-  assert.ok(/api key/i.test(err.message));
+  assert.ok(/422/i.test(err.message));
 });
