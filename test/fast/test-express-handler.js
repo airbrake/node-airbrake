7f05fc34ca33f9c24b4854a5c023214d6bdfd4ab
Jon Keating <jon@licq.org>
2012-07-28 09:31:45 +0900

    Add uncaughtException handler that will kill the process
    Update tests to make sense with how express handles exceptoins in routes

--- test/fast/test-express-handler.js
+++ test/fast/test-express-handler.js
@@ -8,8 +8,6 @@ var http = require('http');
 
 sinon.spy(airbrake, 'notify');
 
-airbrake.dieOnError = false;
-
 app.error(airbrake.expressHandler());
 app.listen(common.port);
 
@@ -19,17 +17,22 @@ app.get('/caught', function(req, res, next) {
 });
 
 app.get('/uncaught', function(req, res, next) {
-  var err = new Error('i am uncaught!');
+  // This actually gets handled by app.error() as well, express will catch
+  // this one for us.
+  var err = new Error('i am quasi uncaught!');
   throw err;
 });
 
 
 process.on('exit', function() {
-  assert.ok(airbrake.notify.called);
+  assert.equal(airbrake.notify.callCount, 3);
+  process.exit();
 });
 
 http.request({port: common.port, path: '/caught'}, function() {
+  assert.equal(airbrake.notify.callCount, 1);
   http.request({port: common.port, path: '/uncaught'}, function () {
-    process.exit();
+    assert.equal(airbrake.notify.callCount, 2);
+    throw new Error('I am really uncaught');
   }).end();
 }).end();
