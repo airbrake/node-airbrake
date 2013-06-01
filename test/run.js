66e1bc44441a31ec93fada1f496b63439da4da12
Carlos Brito Lage <carlos@carloslage.net>
2013-06-01 18:28:50 -0400

    Now able to call test/run.js for specifc test files and with custom verbosity

--- test/run.js
+++ test/run.js
@@ -2,6 +2,25 @@
 var far = require('far').create();
 
 far.add(__dirname);
-far.include(/test-.*\.js$/);
+
+var specificTest = process.argv[2]
+if (undefined !== specificTest) {
+  far.include(new RegExp('test-' + specificTest + '\.js$'));
+} else {
+  far.include(/test-.*\.js$/);
+}
+
+
+var verbosity = process.argv[3]
+if (undefined !== verbosity) {
+  verbosity = Number(verbosity)
+  
+  if (1 !== verbosity && 2 !== verbosity) {
+    console.log('Please provide a verbosity value of 1 or 2')
+    process.exit(1)
+  }
+
+  far.verbose(verbosity)
+} 
 
 far.execute();
