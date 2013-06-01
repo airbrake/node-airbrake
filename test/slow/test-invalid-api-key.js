45f2d7d9960f1071389296d43f762b003b367759
Carlos Brito Lage <carlos@carloslage.net>
2013-06-01 18:40:41 -0400

    Fixed test broken by the environment changes

--- test/slow/test-invalid-api-key.js
+++ test/slow/test-invalid-api-key.js
@@ -1,8 +1,9 @@
 var common = require('../common');
-var airbrake = require(common.dir.root).createClient();
+var airbrake = require(common.dir.root).createClient('invalidkey', 'production');
 var assert = require('assert');
 
 var myErr = new Error('test-notify');
 airbrake.notify(myErr, function(err) {
+  assert.ok(!!err, 'should receive an error object')
   assert.ok(/422/i.test(err.message));
 });
