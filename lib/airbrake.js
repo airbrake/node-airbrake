airbrake/node-airbrake

commit 7325c76ea35b4e257a3623da677c4cea4d645d19

Add express error handler

lib/airbrake.js
@@ -38,22 +38,45 @@ Airbrake.createClient = function(key, env) {
   return instance;
 };
 
+Airbrake.prototype.expressHandler = function() {
+  var self = this;
+
+  return function errorHandler(err, req, res, next) {
+    if (res.statusCode < 400) res.statusCode = 500;
+    err.url = req.url;
+    err.component = req.url;
+    err.action = req.method;
+    err.params = req.params;
+    err.session = req.session;
+
+    self.onError(err);
+    next(err);
+  }
+}
+
+Airbrake.prototype.onError = function(err) {
+  var self = this;
+  self.log('Airbrake: Uncaught exception, sending notification for:');
+  self.log(err.stack);
+
+  self.notify(err, function(notifyErr, url, devMode) {
+    if (notifyErr) {
+      self.log('Airbrake: Could not notify service.');
+      self.log(notifyErr.stack);
+    } else if (url) {
+      self.log('Airbrake: Notified service: ' + url);
+    } else {
+      self.log('Airbrake: Dev mode, did not send.');
+    }
+
+    process.exit(1);
+  });
+}
+
 Airbrake.prototype.handleExceptions = function() {
   var self = this;
   process.on('uncaughtException', function(err) {
-    self.log('Airbrake: Uncaught exception, sending notification for:');
-    self.log(err.stack);
-
-    self.notify(err, function(notifyErr, url) {
-      if (notifyErr) {
-        self.log('Airbrake: Could not notify service.');
-        self.log(notifyErr.stack);
-      } else {
-        self.log('Airbrake: Notified service: ' + url);
-      }
-
-      process.exit(1);
-    });
+    self.onError(err)
   });
 };
 
@@ -66,7 +89,7 @@ Airbrake.prototype.notify = function(err, cb) {
   // log errors instead of posting to airbrake if a dev enviroment
   if (this.developmentEnvironments.indexOf(this.env) != -1) {
     this.log(err);
-    return callback();
+    return callback(null, null, true);
   }
 
   var body = this.notifyXml(err);

commit f1ac2dd58e1ee3d781ef63e0158713248a1b5bce

Fixed reverse-compatibility breakage introduced with 7325c76ea35b4e257a3623da677c4cea4d645d19

lib/airbrake.js
@@ -67,10 +67,10 @@ Airbrake.prototype._onError = function(err, die) {
     if (notifyErr) {
       self.log('Airbrake: Could not notify service.');
       self.log(notifyErr.stack);
-    } else if (url) {
-      self.log('Airbrake: Notified service: ' + url);
-    } else {
+    } else if (devMode) {
       self.log('Airbrake: Dev mode, did not send.');
+    } else {
+      self.log('Airbrake: Notified service: ' + url);
     }
 
     if (die) {