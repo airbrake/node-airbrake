765f75b0b37cf1b171284643197d6d5e9e0ae7dd
Hrvoje Šimić <shime@twobucks.co>
2016-02-23 15:46:11 +0100

    start serializing environment and context for JSON api

--- test/fast/test-host.js
+++ test/fast/test-host.js
@@ -11,29 +11,22 @@ var xmlbuilder = require('xmlbuilder');
 
 (function testPlainHost() {
   var err = new Error('oh no');
-  var notice = xmlbuilder.create().begin('notice');
-
-  airbrake.appendRequestXml(notice, err);
-  var url = notice.children[0].children[0].children[0].value;
+  var url = airbrake.contextJSON(err)["url"];
   assert.equal(url, airbrake.host);
 })();
 
 (function testPartialErrUrl() {
   var err = new Error('oh no');
   err.url = '/foo';
-  var notice = xmlbuilder.create().begin('notice');
+  var url = airbrake.contextJSON(err)["url"];
 
-  airbrake.appendRequestXml(notice, err);
-  var url = notice.children[0].children[0].children[0].value;
   assert.equal(url, airbrake.host + err.url);
 })();
 
 (function testAbsoluteErrUrl() {
   var err = new Error('oh no');
   err.url = 'http://example.org/bar';
-  var notice = xmlbuilder.create().begin('notice');
+  var url = airbrake.contextJSON(err)["url"];
 
-  airbrake.appendRequestXml(notice, err);
-  var url = notice.children[0].children[0].children[0].value;
   assert.equal(url, err.url);
 })();
