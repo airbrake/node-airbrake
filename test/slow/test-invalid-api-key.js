4b7e292d4c34c2ae6a2ba3e58ca094257a878437
Hrvoje Šimić <shime@twobucks.co>
2016-02-29 13:38:43 +0100

    update test for invalid api key

--- test/slow/test-invalid-api-key.js
+++ test/slow/test-invalid-api-key.js
@@ -1,9 +1,9 @@
 var common = require('../common');
-var airbrake = require(common.dir.root).createClient('invalidkey', null, 'production');
+var airbrake = require(common.dir.root).createClient('1234', 'invalid', 'production');
 var assert = require('assert');
 
 var myErr = new Error('test-notify');
 airbrake.notify(myErr, function(err) {
   assert.ok(!!err, 'should receive an error object')
-  assert.ok(/400/i.test(err.message));
+  assert.ok(/401/i.test(err.message));
 });
