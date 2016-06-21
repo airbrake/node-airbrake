0fb12d8d4dd6eb366c37fba526c3406a3c70ab25
Dave Jeffery <dave@davejeffery.com>
2016-06-21 16:49:47 +0100

    Use lodash individual modules instead of requiring the entire package (#102)
    * Use lodash individual modules instead of requiring the entire package
    * replace lodash.assign with lodash.merge

--- test/common.js
+++ test/common.js
@@ -3,7 +3,7 @@
 /* eslint import/no-unresolved: 0 */
 
 var path = require('path');
-var _ = require('lodash');
+var merge = require('lodash.merge');
 
 // An account on the free plan specifically for testing this module.
 exports.key = '96979331ec7e18bbe7ec1529da2ed083';
@@ -11,7 +11,7 @@ exports.projectId = '122374';
 
 // Use custom config if available instead
 try {
-  _.merge(exports, require('./config'));
+  merge(exports, require('./config'));
 } catch (e) { console.log(e); }
 
 exports.port = 8424;
